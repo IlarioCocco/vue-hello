64da703f5dbc87eb15cee9b13fd793f4db19a2f6
IlarioCOCCO <ilario.cocco@gmail.com>
2022-03-18 14:49:25 +0100

    recap vue.js and exercise update

--- js/scripts.js
+++ js/scripts.js
@@ -3,7 +3,6 @@ const app = new Vue(
         el: "#root",//si puo  dare l'id che si vuole
         data: {
             message: "Hello VueJs",
-           
             linkImg: "/img/vueBW.png",
         },
         methods: {
