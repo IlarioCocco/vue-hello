65ed55abf3c285b3f4392d4435fc1d1f539acfcb
IlarioCOCCO <ilario.cocco@gmail.com>
2022-03-18 14:31:54 +0100

    recap vue.js

--- js/scripts.js
+++ js/scripts.js
@@ -3,8 +3,8 @@ const app = new Vue(
         el: "#root",//si puo  dare l'id che si vuole
         data: {
             message: "Hello VueJs",
-            classe: "benvenuto",
-            linkImg: "/img/vue_js_logo.png",
+           
+            linkImg: "/img/vueBW.png",
         },
         methods: {
             saluta: function () {
