f055015e8b69df91213d9bcd3fae2706a5cfee2e
IlarioCOCCO <ilario.cocco@gmail.com>
2022-03-18 18:33:42 +0100

    recap vue.js

--- js/scripts.js
+++ js/scripts.js
@@ -2,15 +2,15 @@ const app = new Vue(
     {
         el: "#root",//si puo  dare l'id che si vuole
         data: {
-            message: "Hello Vue",
+            message: "HELLO VUE",
             linkImg: "/img/vueBW.png",
             classeBottoneBiancoNero: "black_white",
             classeBottoneColore: "color", 
             classeBkgInput: "input",
         },
         methods: {
-            alert: function () {
-                alert(this.message);
+            visual: function () {
+                document.getElementById("vueHello").innerHTML = this.message;
             }
         }
     }
